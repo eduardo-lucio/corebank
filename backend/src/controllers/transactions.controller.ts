0b05d79a61d07e1e5886068c476d748033750dfd
eduardo <yuunoev@gmail.com>
2026-08-27 14:52:17 -0300

    feat: withdraw and deposit

--- backend/src/controllers/transactions.controller.ts
+++ backend/src/controllers/transactions.controller.ts
@@ -1,6 +1,110 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import {pool} from "../../config/db";
 
+export async function withdraw (req: FastifyRequest, res: FastifyReply){
+    const { value } = req.body as { value: number };
+    if(!value || value <= 0){
+        return res.status(400).send({
+            status: 400,
+            error: 'Bad request',
+            message: 'O valor da transacao deve ser maior que zero.'
+        })
+    }
+    const client = await pool.connect();
+    try{
+        let queryResult = await client.query('SELECT * FROM accounts WHERE user_id = $1', [req.user.sub]);
+        if(queryResult.rows.length === 0){
+            return res.code(400).send({
+                status: 400,
+                error: 'No account found.'
+            })
+        }
+        const userAccount = await client.query('SELECT id, status FROM users WHERE id = $1', [req.user.sub]);
+        if(userAccount.rows[0].status !== "ACTIVE"){
+            return res.status(400).send({
+                status: 400,
+                error: 'O dono da conta está com pendencias'
+            })
+        }
+
+        if(value > queryResult.rows[0].balance){
+            return res.status(400).send({
+                status: 400,
+                error: 'Bad request',
+                message: 'Saldo insuficiente'
+            })
+        }
+        await client.query('BEGIN')
+        const withdrawQuery = await client.query('UPDATE accounts SET balance = balance - $1 WHERE account_number = $2 RETURNING id, account_number, balance', [value, queryResult.rows[0].account_number]);
+        const withdrawAccountId = withdrawQuery.rows[0].id;
+        const transactionQuery = await client.query(`INSERT INTO transactions (sender_account_id, receiver_account_id, amount, type, status) VALUES ($1, null, $2, 'WITHDRAW', 'DONE') RETURNING *`,[withdrawAccountId, value]);
+        await client.query('COMMIT')
+        return res.status(200).send({
+            status: 200,
+            message: 'withdraw successfully.',
+            transactionQuery: transactionQuery.rows[0],
+            withdrawQuery: withdrawQuery.rows[0]
+        })
+    }catch(e){
+        await client.query('ROLLBACK')
+        req.log.error(e);
+        return res.status(500).send({
+            status: 500,
+            error: e,
+            message: 'deu erro'
+        })
+    }finally {
+        client.release();
+    }
+}
+
+export async function deposit(req: FastifyRequest, res: FastifyReply){
+    const { value, account } = req.body as {value: number, account: string};
+    if(!value || value <= 0){
+        return res.status(400).send({
+            status: 400,
+            error: 'Bad request',
+            message: 'O valor da transacao deve ser maior que zero.'
+        })
+    }
+    const client = await pool.connect();
+    try{
+        let queryResult = await client.query('SELECT * FROM accounts WHERE account_number = $1', [account]);
+        if(queryResult.rows.length === 0){
+            return res.code(400).send({
+                status: 400,
+                error: 'No account found.'
+            })
+        }
+        const userAccount = await client.query('SELECT id, status FROM users WHERE id = (SELECT user_id FROM accounts WHERE account_number = $1)', [account]);
+        if(userAccount.rows[0].status !== "ACTIVE"){
+            return res.status(400).send({
+                status: 400,
+                error: 'O dono da conta está com pendencias',
+            })
+        }
+        await client.query('BEGIN')
+        const depositQuery = await client.query('UPDATE accounts SET balance = balance + $1 WHERE account_number = $2 RETURNING id, account_number, balance', [value ,account]);
+        const depositAccountId = depositQuery.rows[0].id;
+        const transactionQuery = await client.query(`INSERT INTO transactions (sender_account_id, receiver_account_id, amount, type, status) VALUES (null, $1, $2, 'DEPOSIT', 'DONE') RETURNING *`,[depositAccountId, value]);
+        await client.query('COMMIT')
+        return res.status(200).send({
+            status: 200,
+            message: 'Deposits successfully.',
+            transactionQuery: transactionQuery.rows[0],
+            depositQuery: depositQuery.rows[0]
+        })
+    }catch(e){
+        await client.query('ROLLBACK')
+        return res.status(500).send({
+            status: 500,
+            error: 'Bad request',
+            message: 'deu erro'
+        })
+    }finally {
+        client.release();
+    }
+}
 export async function transfer(req:FastifyRequest, res:FastifyReply) {
     const userId = req.user.sub
     const { receiver_account_number, value } = req.body as {
--- backend/src/routes/transactions.routes.ts
+++ backend/src/routes/transactions.routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
-import { transfer } from '../controllers/transactions.controller';
+import {deposit, transfer, withdraw} from '../controllers/transactions.controller';
 
 export async function transactionRoutes(app: FastifyInstance) {
     app.post('/transactions/transfer', { onRequest: [app.authenticate] }, transfer);
+    app.post('/transactions/deposit', deposit);
+    app.post('/transactions/withdraw',{ onRequest: [app.authenticate] }, withdraw);
 }
